74a612777e8cc9539f4f9931fb05fc75f97c69fd
Yoshi Ueda <88098913+rainedrat@users.noreply.github.com>
2022-03-20 10:53:16 -0400

    Just a note to "copy" the exisitng repository.
    Making a clone of an existing repository to the local drive and then pushing it to the other repository.
    In other words, copying from the GitHub repository X to the local and then copying the local repository X' to the other GitHub repository Y.

---
+++ copy_private_repository.cmd
@@ -0,0 +1,20 @@
+C:\Users\Rainedrat>git clone --bare https://github.com/rainedrat/SOX2T
+# Cloning into bare repository 'SOX2T.git'...
+# info: please complete authentication in your browser...
+# remote: Enumerating objects: 69, done.
+# remote: Counting objects: 100% (69/69), done.
+# remote: Compressing objects: 100% (65/65), done.
+# remote: Total 69 (delta 28), reused 2 (delta 0), pack-reused 0Receiving objects:  65% (45/69)
+# Receiving objects: 100% (69/69), 33.97 KiB | 1.70 MiB/s, done.
+# Resolving deltas: 100% (28/28), done.
+
+C:\Users\Rainedrat\SOX2T.git>git push --mirror https://github.com/HashinoLab/Ueda_et_al_SOX2_tdTomato.git
+# Enumerating objects: 69, done.
+# Counting objects: 100% (69/69), done.
+# Delta compression using up to 4 threads
+# Compressing objects: 100% (37/37), done.
+# Writing objects: 100% (69/69), 33.97 KiB | 33.97 MiB/s, done.
+# Total 69 (delta 28), reused 69 (delta 28), pack-reused 0
+# remote: Resolving deltas: 100% (28/28), done.
+# To https://github.com/HashinoLab/Ueda_et_al_SOX2_tdTomato.git
+#  * [new branch]      main -> main
